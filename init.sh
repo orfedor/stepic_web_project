d6d103b3c5820916087e8650305f72f12c0e07f9
Orfedor <orfedor@yandex.ru>
2017-02-04 14:11:48 +0700

    local

--- init.sh
+++ init.sh
@@ -1,2 +1,2 @@
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo ln -sf /home/fedor/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
