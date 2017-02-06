aabee6b413d0bec5b3aed665d4b3216f8da316cf
Orfedor <orfedor@yandex.ru>
2017-02-06 21:35:17 +0700

    for_local_2

---
+++ hello.py
@@ -0,0 +1,6 @@
+def app(environ, start_response):
+    data = ''
+    for line in environ["QUERY_STRING"].split("&"):
+        data = data+line+"\n"
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return [data]
