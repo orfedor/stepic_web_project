9ea3580675bd9184a50e7fe4309c7fe146d4be72
Orfedor <orfedor@yandex.ru>
2017-02-09 19:54:50 +0700

    dj2

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.test, name='index'),
-    url(r'^login/', views.test, name='login'),
-    url(r'^signup/', views.test, name='signup'),
-    url(r'^question/<\d+>/', views.test, name='question'),
-    url(r'^ask/', views.test, name='ask'),
-    url(r'^popular/', views.test, name='popular'),
-    url(r'^new/', views.test, name='new')
+    url(r'^login/$', views.test, name='login'),
+    url(r'^signup/$', views.test, name='signup'),
+    url(r'^question/<\d+>/$', views.test, name='question'),
+    url(r'^ask/$', views.test, name='ask'),
+    url(r'^popular/$', views.test, name='popular'),
+    url(r'^new/$', views.test, name='new')
 ]
