0e6a60ab650b6a481b7bf80466b9dcdb9799ce1f
Orfedor <orfedor@yandex.ru>
2017-02-13 21:09:29 +0700

    manager

--- bd.sh
+++ bd.sh
@@ -1,4 +1,7 @@
 sudo /etc/init.d/mysql start
 mysql -uroot -e "create database aq1"
-sudo pip3 install virtualenv
+sudo pip3 install django
 source web/bin/activate
+python3 web/askmanage.py makemigrations qa
+python3 web/askmanage.py migrate
+
---
+++ bd.sh~
@@ -0,0 +1,4 @@
+sudo /etc/init.d/mysql start
+mysql -uroot -e "create database aq1"
+sudo pip3 install virtualenv
+source web/bin/activate
